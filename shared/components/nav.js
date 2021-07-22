01339bd6e49ba4bec0b03d6ba67383c39a6d4399
colorlessenergy <brianmunozdev@gmail.com>
2021-07-22 06:35:34 -0700

    update link going to blocks

--- shared/components/nav.js
+++ shared/components/nav.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Nav () {
+    const router = useRouter();
+    const date = router.query.date;
+
     return (
         <div className="flex justify-content-between align-items-center pt-3 mx-15">
             <Link href="/">
@@ -8,7 +12,7 @@ export default function Nav () {
                     home
                 </a>
             </Link>
-            <Link href="/blocks">
+            <Link href={`/day/${ date }/blocks`}>
                 <a>
                     blocks
                 </a>
