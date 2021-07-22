cc2bd2cf3f0c2315a3d63f9262ff19067445b069
colorlessenergy <brianmunozdev@gmail.com>
2021-07-22 05:20:47 -0700

    add HTML structure

--- pages/day/[date].js
+++ pages/day/[date].js
@@ -4,6 +4,33 @@ export default function Date () {
     return (
         <>
             <Nav />
+
+            <p className="mx-15">
+                45 calories
+            </p>
+
+            <div className="card card-light-orange">
+               <div>
+                    <div>
+                        beans
+                    </div>
+                    <div className="text-bold">
+                        100 calories
+                    </div>
+                </div> 
+
+                <div>
+                    <button>
+                        -
+                    </button>
+                    <span className="mx-1">
+                        0 / 3 cans
+                    </span>
+                    <button>
+                        +
+                    </button>
+                </div>
+            </div>
         </> 
     );
 }
