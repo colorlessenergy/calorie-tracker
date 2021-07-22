f5c45c5020f5ff00fd16812b7a61f17b4d19c5ed
colorlessenergy <brianmunozdev@gmail.com>
2021-07-22 11:19:23 -0700

    populate food block forms from localStorage

--- pages/day/[date]/blocks.js
+++ pages/day/[date]/blocks.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 import Nav from '../../../shared/components/nav';
 
+import { getFoodFromLocalStorage } from '../../../shared/food/food';
+
 export default function Blocks () {
+    const router = useRouter();
+    const date = router.query.date;
+    const [ foodBlocks, setFoodBlocks ] = useState([]);
+    useEffect(() => {
+        if (date) {
+            setFoodBlocks((getFoodFromLocalStorage(date)).blocks);
+        }
+    }, [ date ]);
+
     return (
         <div className="container">
             <Nav />
@@ -12,56 +26,67 @@ export default function Blocks () {
                 405 calories
             </div>
 
-            <div className="ribbon"></div>
-            <form className="flex justify-content-between mx-15">
-               <div className="flex flex-direction-column align-items-start form-groups-container">
-                    <label htmlFor="food">
-                        food
-                    </label>
-                    <input
-                        type="text"
-                        id="food" />
+            { foodBlocks.map((foodBlock, index) => {
+                return (
+                    <div key={index}>
+                        <div className="ribbon"></div>
+                        <form className="flex justify-content-between mx-15">
+                            <div className="flex flex-direction-column align-items-start form-groups-container">
+                                <label htmlFor="food">
+                                    food
+                                </label>
+                                <input
+                                    value={ foodBlocks[index].name }
+                                    type="text"
+                                    id="food" />
 
-                    <label htmlFor="calories">
-                        calories
-                    </label>
-                    <input
-                        type="number"
-                        id="calorie" />
+                                <label htmlFor="calories">
+                                    calories
+                                </label>
+                                <input
+                                    value={ foodBlocks[index].calories }
+                                    type="number"
+                                    id="calorie" />
 
-                    <label htmlFor="increment">
-                        increment
-                    </label>
-                    <input
-                        type="number"
-                        id="increment" />
+                                <label htmlFor="increment">
+                                    increment
+                                </label>
+                                <input
+                                    value={ foodBlocks[index].increment }
+                                    type="number"
+                                    id="increment" />
 
-                    <label htmlFor="unit">
-                        unit
-                    </label>
-                    <input
-                        type="text"
-                        id="unit" />
+                                <label htmlFor="unit">
+                                    unit
+                                </label>
+                                <input
+                                    value={ foodBlocks[index].unit }
+                                    type="text"
+                                    id="unit" />
 
-                    <label htmlFor="limit">
-                        limit
-                    </label>
-                    <input
-                        type="text"
-                        id="limit" />
-                </div> 
+                                <label htmlFor="limit">
+                                    limit
+                                </label>
+                                <input
+                                    value={ foodBlocks[index].limit }
+                                    type="number"
+                                    id="limit" />
+                            </div> 
 
-                <div className="flex flex-direction-column">
-                    <button
-                        type="button"
-                        className="button button-red mb-2">
-                        remove
-                    </button>
-                    <button className="button button-green">
-                        update
-                    </button>
-                </div>
-            </form>
+                            <div className="flex flex-direction-column">
+                                <button
+                                    type="button"
+                                    className="button button-red mb-2">
+                                    remove
+                                </button>
+                                <button className="button button-green">
+                                    update
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                );
+            }) }
         </div>
     );
 }
