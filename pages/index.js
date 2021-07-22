506302872e2c37f44bdcf3db986e00cddbaa9b88
colorlessenergy <brianmunozdev@gmail.com>
2021-07-21 22:28:52 -0700

    add HTML structure to home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>calorie tracker</title>
-        <meta name="description" content="calorie tracker" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import Calender from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-      <h1>calorie tracker</h1>
-    </div>
-  );
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>calorie tracker</title>
+                <meta name="description" content="calorie tracker" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1 className="my-0 pt-3 text-center">calorie tracker</h1>
+            <p className="my-0 text-center">track calories for a day</p>
+
+            <Calender className="mt-5 m-center" />
+        </div>
+    );
 }
