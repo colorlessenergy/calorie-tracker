62de2f6e7afea4eb4c3f5d3ce70f360f011b0524
colorlessenergy <brianmunozdev@gmail.com>
2021-07-21 22:36:26 -0700

    add functionality to redirect to date when clicking on a date

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router';
 
 import Calender from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 export default function Home() {
+    const router = useRouter();
+    const onChange = (date) => {
+        date = date.toLocaleDateString('en-US').replace(/\//g, '-');
+        router.push(`day/${ date }`);
+    }
+
     return (
         <div>
             <Head>
@@ -15,7 +22,9 @@ export default function Home() {
             <h1 className="my-0 pt-3 text-center">calorie tracker</h1>
             <p className="my-0 text-center">track calories for a day</p>
 
-            <Calender className="mt-5 m-center" />
+            <Calender
+                onChange={ onChange }
+                className="mt-5 m-center" />
         </div>
     );
 }
