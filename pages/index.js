e9c5db52f1eec1b3d0f8f3057a4b4f37ebe1f4f4
colorlessenergy <brianmunozdev@gmail.com>
2021-08-03 14:53:32 -0700

    remove functionality to skip waiting

--- pages/index.js
+++ pages/index.js
@@ -15,25 +15,6 @@ export default function Home() {
         router.push(`day/${ date }`);
     }
 
-    useEffect(() => {
-        if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
-            const wb = window.workbox
-            const promptNewVersionAvailable = () => {
-                if (confirm('A newer version of this web app is available, reload to update?')) {
-                    wb.addEventListener('controlling', () => {
-                        window.location.reload()
-                    });
-
-                    wb.messageSkipWaiting();
-                } 
-            }
-
-            wb.addEventListener('waiting', promptNewVersionAvailable);
-
-            wb.register();
-        }
-    }, []);
-
     return (
         <div>
             <Head>
