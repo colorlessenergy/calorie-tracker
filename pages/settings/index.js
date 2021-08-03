9aaf4f846da756740d50939ae76cf52cf5bb7731
colorlessenergy <brianmunozdev@gmail.com>
2021-08-03 09:05:52 -0700

    add HTML structure and settings item component

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -1,9 +1,15 @@
+import SettingsItem from '../../shared/components/SettingsItems/SettingsItem';
+
 export default function Settings() {
     return (
-        <div>
-            <h1>
-                settings
-            </h1>
+        <div className="container">
+            <div className="mx-15">
+                <h1>
+                    settings
+                </h1>
+
+                <SettingsItem text="data" link="/settings/data" />
+            </div>
         </div>
     );
 }
