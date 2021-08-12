421adf778272bfcd9dd2779dc2de6e6323428335
colorlessenergy <brianmunozdev@gmail.com>
2021-08-12 09:32:31 -0700

    add functionality to automatically update service worker

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,22 @@ export default function Home() {
         router.push(`day/${ date }`);
     }
 
+    useEffect(() => {
+        if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined) {
+            const wb = window.workbox;
+            const installNewVersion = () => {
+                wb.addEventListener('controlling', () => {
+                    window.location.reload();
+                });
+                
+                wb.messageSkipWaiting();
+            }
+
+            wb.addEventListener('waiting', installNewVersion);
+            wb.register();
+        }
+    }, []);
+
     return (
         <div>
             <Head>
