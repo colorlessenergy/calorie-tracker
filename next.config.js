9ff959f2ff8580134d847f09c6749d1d1b45156d
colorlessenergy <brianmunozdev@gmail.com>
2021-08-13 10:40:20 -0700

    uncomment react strict mode

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-    // reactStrictMode: true,
+    reactStrictMode: true,
     pwa: {
         dest: 'public',
         skipWaiting: false
