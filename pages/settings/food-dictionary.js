8d2dfbf913000213cb901bb12de61859076131bd
colorlessenergy <brianmunozdev@gmail.com>
2022-01-11 21:08:04 -0800

    move food blocks outside div

--- pages/settings/food-dictionary.js
+++ pages/settings/food-dictionary.js
@@ -112,50 +112,49 @@ export default function foodDictionary () {
                     <button
                         onClick={ addNewFoodBlock }
                         className="button button-green mb-1">add food</button>
+                </div>
 
-
-                    <div className="flex flex-wrap justify-content-between">
-                        { foodDictionary.length ? (foodDictionary.map(foodBlock => {
-                            return (
+                <div className="flex flex-wrap justify-content-between">
+                    { foodDictionary.length ? (foodDictionary.map(foodBlock => {
+                        return (
+                            <div
+                                className="card text-medium"
+                                key={ foodBlock.ID }>
                                 <div
-                                    className="card text-medium"
-                                    key={ foodBlock.ID }>
-                                    <div
-                                        onClick={ () => toggleEditFoodBlockModal(foodBlock) }
-                                        className="text-bold cursor-pointer">   
-                                        { foodBlock.name } 
-                                    </div>
-                                    <div
-                                        onClick={ () => toggleEditFoodBlockModal(foodBlock) }
-                                        className="flex justify-content-between w-100 mb-1 cursor-pointer">
-                                        <div>
-                                            { foodBlock.calories } calories
-                                        </div>
-
-                                        <div>
-                                            { foodBlock.amount } { foodBlock.unit } 
-                                        </div>
-                                    </div>
-
-                                    <div className="align-self-start w-100">
-                                        <label htmlFor={`calculateCalories-${ foodBlock.ID }`}>calculate calories</label>
-                                        <input
-                                            onChange={ event => handleCalculateCalories({ value: event.currentTarget.value, foodBlock }) }
-                                            type="number"
-                                            id={`calculateCalories-${ foodBlock.ID }`}
-                                            name={`calculateCalories-${ foodBlock.ID }`}
-                                            min="1"
-                                            step="0.01"
-                                            placeholder="amount" />
-                                    </div>
-
-                                    { amountOfCaloriesPerUnit[ foodBlock.ID ] ? (
-                                        <div className="align-self-start">{ amountOfCaloriesPerUnit[ foodBlock.ID ] } calories</div>
-                                    ) : (null) }
+                                    onClick={ () => toggleEditFoodBlockModal(foodBlock) }
+                                    className="text-bold cursor-pointer">   
+                                    { foodBlock.name } 
                                 </div>
-                            );
-                        })) : (null) }
-                    </div>
+                                <div
+                                    onClick={ () => toggleEditFoodBlockModal(foodBlock) }
+                                    className="flex justify-content-between w-100 mb-1 cursor-pointer">
+                                    <div>
+                                        { foodBlock.calories } calories
+                                    </div>
+
+                                    <div>
+                                        { foodBlock.amount } { foodBlock.unit } 
+                                    </div>
+                                </div>
+
+                                <div className="align-self-start w-100">
+                                    <label htmlFor={`calculateCalories-${ foodBlock.ID }`}>calculate calories</label>
+                                    <input
+                                        onChange={ event => handleCalculateCalories({ value: event.currentTarget.value, foodBlock }) }
+                                        type="number"
+                                        id={`calculateCalories-${ foodBlock.ID }`}
+                                        name={`calculateCalories-${ foodBlock.ID }`}
+                                        min="1"
+                                        step="0.01"
+                                        placeholder="amount" />
+                                </div>
+
+                                { amountOfCaloriesPerUnit[ foodBlock.ID ] ? (
+                                    <div className="align-self-start">{ amountOfCaloriesPerUnit[ foodBlock.ID ] } calories</div>
+                                ) : (null) }
+                            </div>
+                        );
+                    })) : (null) }
                 </div>
             </div>
 
