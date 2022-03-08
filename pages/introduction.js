9bcbe110541af1f415491b3b1d4be040706e9934
colorlessenergy <brianmunozdev@gmail.com>
2022-03-08 08:59:25 -0800

    redirect to home page if calorie goal is set

--- pages/introduction.js
+++ pages/introduction.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Introduction () { 
+    const router = useRouter();
+    useEffect(() => {
+        if (localStorage.getItem('calorieGoal')) {
+            return router.push('/');
+        }
+
+        localStorage.setItem('calorieGoal', '1');
+    }, []);
+
     return (
         <div className="container mx-15">
             <p>
