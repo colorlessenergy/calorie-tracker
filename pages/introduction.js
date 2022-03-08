175aded8c098178f8c404950c3d41beb854fa0d3
colorlessenergy <brianmunozdev@gmail.com>
2022-03-08 09:23:36 -0800

    add functionality to set calorie goal and redirect to home page

--- pages/introduction.js
+++ pages/introduction.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export default function Introduction () { 
@@ -11,6 +11,21 @@ export default function Introduction () {
         localStorage.setItem('calorieGoal', '1');
     }, []);
 
+    const [ calorieGoal, setCalorieGoal ] = useState(1);
+    const handleInputChange = (event) => {
+        if (!event.currentTarget.valueAsNumber) return setCalorieGoal('');
+
+        setCalorieGoal(event.currentTarget.valueAsNumber);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        localStorage.setItem('calorieGoal', calorieGoal);
+
+        router.push('/');
+    }
+
     return (
         <div className="container">
             <div className="mx-15">
@@ -19,15 +34,17 @@ export default function Introduction () {
                     input a calorie goal below to get started. The calorie goal can be updated in the settings.
                 </p>
 
-                <form className="w-50-md">
+                <form
+                    onSubmit={ handleSubmit }
+                    className="w-50-md">
                     <div>
                         <label htmlFor="calorieGoal">calorie goal</label>
                         <input
                             type="number"
                             id="calorieGoal"
                             name="calorieGoal"
-                            min="1"
-                            defaultValue="1"
+                            onChange={ handleInputChange }
+                            value={ calorieGoal }
                             required
                             placeholder="calorie goal" />
                     </div>
