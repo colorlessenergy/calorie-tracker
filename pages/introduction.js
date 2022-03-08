f5795ad14692e2060be848d214df5a850e7341c6
colorlessenergy <brianmunozdev@gmail.com>
2022-03-08 08:55:02 -0800

    redirect to introduction page if calorie goal is not set

---
+++ pages/introduction.js
@@ -0,0 +1,9 @@
+export default function Introduction () { 
+    return (
+        <div className="container mx-15">
+            <p>
+                hello
+            </p>
+        </div>
+    );
+}
