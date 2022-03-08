3172110f19c4f425a4778ec864363661f068bdb8
colorlessenergy <brianmunozdev@gmail.com>
2022-03-08 09:11:10 -0800

    add HTML structure

--- pages/introduction.js
+++ pages/introduction.js
@@ -12,10 +12,30 @@ export default function Introduction () {
     }, []);
 
     return (
-        <div className="container mx-15">
-            <p>
-                hello
-            </p>
+        <div className="container">
+            <div className="mx-15">
+                <h1>welcome to calorie tracker</h1>
+                <p className="text-medium">
+                    input a calorie goal below to get started. The calorie goal can be updated in the settings.
+                </p>
+
+                <form className="w-50-md">
+                    <div>
+                        <label htmlFor="calorieGoal">calorie goal</label>
+                        <input
+                            type="number"
+                            id="calorieGoal"
+                            name="calorieGoal"
+                            min="1"
+                            defaultValue="1"
+                            required
+                            placeholder="calorie goal" />
+                    </div>
+
+                    <button
+                        className="button button-green">add</button>
+                </form>
+            </div>
         </div>
     );
 }
