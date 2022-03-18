443f5af9182501e1f3ac1530b9e708cc721cc532
colorlessenergy <brianmunozdev@gmail.com>
2022-03-17 22:27:23 -0700

    add flex wrap style

--- pages/day/[date]/index.js
+++ pages/day/[date]/index.js
@@ -209,7 +209,7 @@ export default function Date () {
         <div className="container">
             <Nav link={{ link: `/day/${ date }`, text: date }} />
 
-            <div className="mx-15 mt-1 mb-1 flex align-items-center">
+            <div className="mx-15 mt-1 mb-1 flex align-items-center flex-wrap">
                 <div className="mr-1">
                     { totalCalories } total calories
                 </div>
